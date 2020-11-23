d90878fae23a73d4b6c66efda74598e6d703a0b0
Marina Pereira Alves <65171490+AlvesMarina@users.noreply.github.com>
2020-11-22 21:16:46 -0300

    Add files via upload

---
+++ AloMundo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+ int main(){
+    printf("Anderson Luiz\n\nMarina Alves\n\nTayna Crisllen\n");
+    printf("\n%d\n", (6+6+1));
+    printf("\nAlo, mundo!\n");
+
+    return 0;
+ }
